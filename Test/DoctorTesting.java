262ea4f14dbcea833cdad2cd67cdf14858be2081
ShashankaDhungana <shashanka.dd@gmail.com>
2022-02-04 22:03:39 +0545

    commit

---
+++ Test/DoctorTesting.java
@@ -0,0 +1,20 @@
+// import Controller.DoctorController;
+// import View.Registration;
+// import model.Doctor;
+// import org.junit.After;
+// import org.junit.Test;
+// import org.junit.Assert;
+
+// import java.util.ResourceBundle;
+
+// public class DoctorTesting {
+// @Test
+// public void registration(){
+// Doctor newDoctor = new Doctor("Paras", "Ayer", "986012345", "Baneshwor", "paras", "password");
+// DoctorController doctorController = new DoctorController();
+// Assert.assertEquals(1, doctorController.registerDoctor(newDoctor));
+// }
+// }
+
+
+
